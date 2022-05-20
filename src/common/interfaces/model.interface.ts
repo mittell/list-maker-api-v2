a77e81998eb41a3f71fdd5049866aee0c3250958
Chris Mittell <mittell@hotmail.co.uk>
2022-05-20 15:55:47 +0900

    feat: updated interface references

--- src/common/interfaces/model.interface.ts
+++ src/common/interfaces/model.interface.ts
@@ -1,9 +1,11 @@
 import { IContext } from '../context/context.interface';
-import { IDto } from './dto.interface';
+import { ICreateDto } from './createDto.interface';
+import { IPatchDto } from './patchDto.interface';
+import { IPutDto } from './putDto.interface';
 
 export interface IModel {
-	mapFromCreateDto(dto: IDto): Promise<void>;
-	mapFromPutDto(dto: IDto): Promise<void>;
-	mapFromPatchDto(dto: IDto): Promise<void>;
+	mapFromCreateDto(dto: ICreateDto): Promise<void>;
+	mapFromPutDto(dto: IPutDto): Promise<void>;
+	mapFromPatchDto(dto: IPatchDto): Promise<void>;
 	getModel(dbContext: IContext): Promise<any>;
 }
